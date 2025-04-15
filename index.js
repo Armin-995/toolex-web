5fdaed28fc83d20054f7e244122868e759a75649
Armin <a.said@t-online.de>
2025-04-15 16:13:48 +0200

    Def

---
+++ index.js
@@ -0,0 +1,17 @@
+let farben = ['rot', 'blau'];
+farben[2,3]='steifn','icke';
+function greet(name){
+    
+    console.log('Hey' + name);
+}
+
+function square(number){
+
+    return number*number;
+
+}
+greet('Du');
+console.log(square(16));
+
+
+
